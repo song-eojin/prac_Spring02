626576c831abab2b1160d73e0a18f31f09ccb881
song eojin <feshed528@gmail.com>
2023-06-14 02:28:47 +0900

    commit HTTP 데이터를 객체로 처리하는 방법

---
+++ src/main/java/com/sparta/springmvc/request/Star.java
@@ -0,0 +1,16 @@
+package com.sparta.springmvc.request;
+
+import lombok.Getter;
+
+@Getter
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Star() {}
+}
